38887cfc01f0093b63bb59ee5654973bfa7f0011
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2018-05-01 20:10:47 +0800

    state properties are now public.

--- src/mp2/states/State.java
+++ src/mp2/states/State.java
@@ -2,9 +2,9 @@ package mp2.states;
 
 public class State {
 
-	int vertexFrom;
-	int vertexTo;
-	char symbol;
+	public int vertexFrom;
+	public int vertexTo;
+	public char symbol;
 	
 	public State(int vertexFrom, int vertexTo, char symbol) {
 		this.vertexFrom = vertexFrom;
