87bda18ebf888674c27dc1ff2e0ff1d5a73446fc
Michael Loewe Alivio <loewe_alivio@yahoo.com>
2018-05-31 21:29:30 +0800

    code preparations

--- src/mp3/Project.java
+++ src/mp3/Project.java
@@ -1,33 +1,56 @@
 package mp3;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 import mp3.resources.turingMachine.TuringMachine;
 
 public class Project {
-	private static final String FILENAME = "src/mp3/power";
+	private static final String POWER = "src/mp3/power";
+	private static final String REVERSE = "src/mp3/reverse";
 
 	public static void main(String[] args) {
 		try {
-
-			// Import addition machine
-			TuringMachine power = TuringMachine.inParser(FILENAME);
-
-			// Print addition machine
-//			System.out.println("Machine content:");
-//			System.out.println(power);
-
-			// Process input
-			power.process("111#11");
-
-			// Print the tape content after the process
-			System.out.println("Tape content after processing '111#11':");
-			System.out.println(power.getTapeSnapshot());
-
+			computePower();
+			
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 
+		
+	}
+	
+	public static void computePower() throws FileNotFoundException{
+		// Import addition machine
+		TuringMachine power = TuringMachine.inParser(POWER);
+
+		// Print addition machine
+//		System.out.println("Machine content:");
+//		System.out.println(power);
+
+		// Process input
+		power.process("11#1111");
+		
+		// Print the tape content after the process
+		System.out.println("Tape content after processing '11#11':");
+		System.out.println(power.getTapeSnapshot());
+		
+	}
+	
+	public static void doReverse() throws FileNotFoundException {
+		// Import addition machine
+		TuringMachine reverse = TuringMachine.inParser(REVERSE);
+
+		//Scanner sc = new Scanner(System.in);
+		// Process input
+		reverse.process("aa");
+		
+		// Print the tape content after the process
+		System.out.println("Tape content after processing '11#11':");
+		System.out.println(reverse.getTapeSnapshot());
+		
 	}
 
 }
