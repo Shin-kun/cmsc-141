a077c0f431a4cf5917f723ca3b0c585f7b578d2d
Mary Danielle C. Amora <mcamora@up.edu.ph>
2018-06-01 10:20:32 +0800

    Update Project.java

--- src/mp3/Project.java
+++ src/mp3/Project.java
@@ -1,9 +1,7 @@
 package mp3;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
-
 import mp3.resources.turingMachine.TuringMachine;
 
 public class Project {
@@ -11,18 +9,71 @@ public class Project {
 	private static final String REVERSE = "src/mp3/reverse";
 
 	public static void main(String[] args) {
-		try {
-			computePower();
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
 
-		
+	    while (true){
+
+            int choice = 1;
+
+            System.out.println("Menu:\n1. Power\n2. Reverse String\n\nChoice: ");
+            Scanner scan = new Scanner(System.in);
+            choice = scan.nextInt();
+
+            if (choice == 1){
+                String equation = "";
+                int x, y;
+                System.out.println("Compute for power: x^y");
+                System.out.print("Enter x: ");
+                x = scan.nextInt();
+                System.out.print("Enter y: ");
+                y = scan.nextInt();
+                System.out.println("Equation: "+x+"^"+y);
+
+
+                for (int ex = 0; ex < x; ex++){
+                    equation = equation+"1";
+                }
+                equation = equation+"#";
+
+                for (int way = 0; way < y; way++){
+                    equation = equation+"1";
+                }
+
+                System.out.println(equation);
+                try {
+                    computePower(equation);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }else if (choice == 2){
+
+                System.out.println("Enter string: ");
+                Scanner sc = new Scanner(System.in);
+                String equation = sc.nextLine();
+
+                try {
+                    doReverse(equation);
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            System.out.println("PROCESS DONE");
+            System.out.println("Process again? Press 1 for yes and 2 for no");
+            if (scan.nextInt() == 1){
+                //do nothing
+            }else{
+                System.out.println("bye!");
+                break;
+            }
+
+        }
 	}
 	
-	public static void computePower() throws FileNotFoundException{
+	public static void computePower(String equation) throws FileNotFoundException{
 		// Import addition machine
 		TuringMachine power = TuringMachine.inParser(POWER);
 
@@ -31,24 +82,29 @@ public class Project {
 //		System.out.println(power);
 
 		// Process input
-		power.process("11#1111");
+        power.process(equation);
 		
 		// Print the tape content after the process
-		System.out.println("Tape content after processing '11#11':");
+		System.out.println("Final Tape content after processing for equation "+equation+":");
 		System.out.println(power.getTapeSnapshot());
 		
 	}
 	
-	public static void doReverse() throws FileNotFoundException {
+	public static void doReverse(String equation) throws FileNotFoundException {
 		// Import addition machine
-		TuringMachine reverse = TuringMachine.inParser(REVERSE);
+
+
+
+        TuringMachine reverse = TuringMachine.inParser(REVERSE);
+
+
 
 		//Scanner sc = new Scanner(System.in);
 		// Process input
-		reverse.process("aa");
+		reverse.process(equation);
 		
 		// Print the tape content after the process
-		System.out.println("Tape content after processing '11#11':");
+		System.out.println("Tape content after processing :"+equation);
 		System.out.println(reverse.getTapeSnapshot());
 		
 	}
